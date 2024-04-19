fbb7634eb05be7555119e07332db80da8041ba07
jainishikka <jainishikka999@gmail.com>
2024-04-19 23:03:05 +0530

    uploading keeper app

---
+++ src/components/App.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const localStorageKey="notes";
+  const [notes, setNotes] = useState(()=>{
+    return JSON.parse(localStorage.getItem(localStorageKey))
+    ||[]
+  });
+
+  useEffect(()=>{
+    localStorage.setItem(localStorageKey,JSON.stringify(notes))
+  },[notes]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
